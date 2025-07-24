6a57e4add8993402db13717d318febd6b4334202
Dzima-G <dgukalin@gmail.com>
2025-07-24 20:01:29 +0300

    added user registration

---
+++ users/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUser(AbstractUser):
+    phone = PhoneNumberField(
+        verbose_name='Мобильный номер',
+        region='RU',
+        unique=True
+    )
+    address = models.CharField(
+        'Адрес',
+        max_length=250
+    )
+    photo = models.ImageField(
+        'Фото',
+        upload_to='photos/',
+        default = 'photos/unnamed.jpg',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
---
+++ users/views.py
@@ -0,0 +1,40 @@
+from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
+from users.forms import CustomUserCreationForm
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('storage:my_rent')
+    else:
+        form = CustomUserCreationForm()
+    return render(
+        request,
+        'register.html',
+        {'form': form}
+    )
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('storage:my_rent')
+        else:
+            messages.error(request, 'Неверный логин или пароль')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('storage:index')
